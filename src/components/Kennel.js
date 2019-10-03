01fa22f4cf2e98b9eb35fdd469c6a3f0d605ac1a
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-10-03 15:36:39 -0500

    added logout button

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -25,7 +25,14 @@ class Kennel extends Component {
       user: this.isAuthenticated()
     });
   }
+  clearUser = () => {
+    localStorage.clear()
 
+    this.setState({
+        user: this.isAuthenticated()
+    });
+
+}
   componentDidMount() {
     this.setState({
       user: this.isAuthenticated()
@@ -35,7 +42,7 @@ class Kennel extends Component {
   render() {
     return (
       <>
-        <NavBar user={this.state.user} />
+        <NavBar user={this.state.user} clearUser={this.clearUser} />
         <ApplicationViews user={this.state.user}
           setUser={this.setUser} />
       </>
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 import './NavBar.css'
+
 
 // an example is className="nav-link" to"/taco" but the link in NavBar.js must correspond to taco
 class NavBar extends Component {
-
-  render(){
+  handleLogout = () => {
+    this.props.clearUser();
+    this.props.history.push('/');
+  }
+  render() {
 
     return (
       <header>
@@ -15,15 +19,16 @@ class NavBar extends Component {
         <nav>
           <ul className="container">
             <li><Link className="nav-link" to="/">Home</Link></li>
-            {(this.props.user) ?
-            <>
-            <li><Link className="nav-link" to="/animals">Animals</Link></li>
-            <li><Link className="nav-link" to="/Employee">Employees</Link></li>
-            </>
-            : null }
             <li><Link className="nav-link" to="/Location">Locations</Link></li>
             <li><Link className="nav-link" to="/Owner">Owners</Link></li>
-            <li><Link className="nav-link" to="/login">Login</Link></li>
+            {(this.props.user) ?
+              <>
+                <li><Link className="nav-link" to="/animals">Animals</Link></li>
+                <li><Link className="nav-link" to="/Employee">Employees</Link></li>
+                <li><span className="nav-link" onClick={this.handleLogout}>Logout</span></li>
+              </>
+              : <li><Link className="nav-link" to="/login">Login</Link></li>}
+            {/* <li><Link className="nav-link" to="/login">Login</Link></li> */}
           </ul>
         </nav>
       </header>
@@ -31,4 +36,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
